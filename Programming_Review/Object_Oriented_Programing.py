70e3fd43a344dde93a051b4a6655d04157ff1a60
Peter Tiszavolgyi <tiszavolgyi@gmail.com>
2017-06-07 14:33:31 -0700

    First lesson

---
+++ Programming_Review/Object_Oriented_Programing.py
@@ -0,0 +1,8 @@
+class Progtam():
+    def __init__(self, *args, **kwargs):
+
+        self.lang = input("What Language?: ")
+        self.version = float(input("Version?: "))
+        self.skill = input("What skill levele?: ")
+
+p1 = Program()
